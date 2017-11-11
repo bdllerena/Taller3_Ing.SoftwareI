cea0d509ece69b5980322c79716d9c27b222f392
LizethCarolinaGuevaraJimenez <30345693+LizethCarolinaGuevaraJimenez@users.noreply.github.com>
2017-11-10 21:17:54 -0500

    Taller3_PruebasUnitarias.Net

---
+++ Araujo_Guevara/Taller/Taller/CalculoImpuesto.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace Taller
+{
+    public class CalculoImpuesto
+    {
+        public CalculoImpuesto()
+        {
+
+        }
+        public double Sueldo(double sueldo)
+        {
+            if (sueldo < 0 || sueldo > 10000000)
+            {
+
+                return 0;
+            }
+            else
+            {
+                    double sueldoTotal = sueldo * 0.9055 * 12;
+                    return sueldoTotal;
+             }
+
+            //return sueldoFinal;
+        }
+        public double calcularImpueto(int sueldo)
+        {
+            double sueldoExcedente = 0;
+            if (sueldo < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                
+                if (sueldo >= 0 && sueldo < 11290)
+                {
+                    sueldoExcedente = 0;
+                }
+                if (sueldo >= 11290 && sueldo < 14390)
+                {
+                    sueldoExcedente = (sueldo - 11290) * 0.05;
+                }
+                if (sueldo >= 14390 && sueldo < 17990)
+                {
+                    sueldoExcedente = (sueldo - 14390) * 0.10 + 155;
+                }
+                if (sueldo >= 17990 && sueldo < 21600)
+                {
+                    sueldoExcedente = (sueldo - 17990) * 0.12 + 515;
+                }
+                if (sueldo >= 21600 && sueldo < 43190)
+                {
+                    sueldoExcedente = (sueldo - 21600) * 0.15 + 948;
+                }
+                if (sueldo >= 43190 && sueldo < 64770)
+                {
+                    sueldoExcedente = (sueldo - 43190) * 0.20 + 4187;
+                }
+                if (sueldo >= 64770 && sueldo < 86370)
+                {
+                    sueldoExcedente = (sueldo - 64770) * 0.25 + 8503;
+                }
+                if (sueldo >= 86370 && sueldo < 115140)
+                {
+                    sueldoExcedente = (sueldo - 86370) * 0.30 + 13903;
+                }
+                if (sueldo >= 115140)
+                {
+                    sueldoExcedente = (sueldo - 115140) * 0.35 + 22534;
+                }
+            }
+            return sueldoExcedente;
+        }
+    }
+}
---
+++ Araujo_Guevara/Taller/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Taller;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CalcularSueldoUno()
+        {
+            CalculoImpuesto calculo = new CalculoImpuesto();
+            int expectedR = 3803;//valor que se espera 3803
+            int actualR;
+            actualR = (int)calculo.Sueldo(350);
+            Assert.AreEqual(expectedR,actualR);
+        }
+        [TestMethod]
+        public void CalcularSueldoDos()
+        {
+            CalculoImpuesto calculo = new CalculoImpuesto();
+            int expectedR = 10866;//sueldo esperado 10866
+            int actualR;
+            actualR = (int)calculo.Sueldo(1000);
+            Assert.AreEqual(expectedR, actualR);
+        }
+        [TestMethod]
+        public void CalcularSueldoTres()
+        {
+            CalculoImpuesto calculo = new CalculoImpuesto();
+            int expectedR = 11952;//sueldo esperado 11952
+            int actualR;
+            actualR = (int)calculo.Sueldo(1100);
+            Assert.AreEqual(expectedR, actualR);
+        }
+        [TestMethod]
+        public void CalcularSueldoCuatro()
+        {
+            CalculoImpuesto calculo = new CalculoImpuesto();
+            int expectedR = 27165;//sueldo esperado 27165
+            int actualR;
+            actualR = (int)calculo.Sueldo(2500);
+            Assert.AreEqual(expectedR, actualR);
+        }
+        [TestMethod]
+        public void CalcularNumerosNegativos()
+        {
+            CalculoImpuesto calculo = new CalculoImpuesto();
+            int expectedR = 0;//el valor que espera es 0
+            int actualR;
+            actualR = (int)calculo.Sueldo(-2500);
+            Assert.AreEqual(expectedR, actualR);
+        }
+        
+        [TestMethod]
+        public void CalcularImpuestoRan1()
+        {
+            CalculoImpuesto calculo = new CalculoImpuesto();
+            int expectedR = 0;
+            int actualR;
+            actualR = (int)calculo.calcularImpueto(3803);
+            Assert.AreEqual(expectedR, actualR);
+        }
+        [TestMethod]
+        public void CalcularImpuestoRan2()
+        {
+            CalculoImpuesto calculo = new CalculoImpuesto();
+            int expectedR = 35;
+            int actualR;
+            actualR = (int)calculo.calcularImpueto(12000);
+            Assert.AreEqual(expectedR, actualR);
+        }
+        [TestMethod]
+        public void CalcularImpuestoRan3()
+        {
+            CalculoImpuesto calculo = new CalculoImpuesto();
+            int expectedR = 867;
+            int actualR;
+            actualR = (int)calculo.calcularImpueto(15100);
+            Assert.AreEqual(expectedR, actualR);
+        }
+        [TestMethod]
+        public void CalcularImpuestoRan4()
+        {
+            CalculoImpuesto calculo = new CalculoImpuesto();
+            int expectedR = 876;
+            int actualR;
+            actualR = (int)calculo.calcularImpueto(21000);
+            Assert.AreEqual(expectedR, actualR);
+        }
+        [TestMethod]
+        public void CalcularImpuestoRan5()
+        {
+            CalculoImpuesto calculo = new CalculoImpuesto();
+            int expectedR = 3483;
+            int actualR;
+            actualR = (int)calculo.calcularImpueto(38500);
+            Assert.AreEqual(expectedR, actualR);
+        }
+        [TestMethod]
+        public void CalcularImpuestoRan6()
+        {
+            CalculoImpuesto calculo = new CalculoImpuesto();
+            int expectedR = 5549;
+            int actualR;
+            actualR = (int)calculo.calcularImpueto(50000);
+            Assert.AreEqual(expectedR, actualR);
+        }
+        [TestMethod]
+        public void CalcularImpuestoRan7()
+        {
+            CalculoImpuesto calculo = new CalculoImpuesto();
+            int expectedR = 9810;
+            int actualR;
+            actualR = (int)calculo.calcularImpueto(70000);
+            Assert.AreEqual(expectedR, actualR);
+        }
+        [TestMethod]
+        public void CalcularImpuestoRan8()
+        {
+            CalculoImpuesto calculo = new CalculoImpuesto();
+            int expectedR = 17992;
+            int actualR;
+            actualR = (int)calculo.calcularImpueto(100000);
+            Assert.AreEqual(expectedR, actualR);
+        }
+        [TestMethod]
+        public void CalcularImpuestoRan9()
+        {
+            CalculoImpuesto calculo = new CalculoImpuesto();
+            int expectedR = 22835;
+            int actualR;
+            actualR = (int)calculo.calcularImpueto(116000);
+            Assert.AreEqual(expectedR, actualR);
+        }
+        [TestMethod]
+        public void CacularNegativoRango()
+        {
+            CalculoImpuesto calculo = new CalculoImpuesto();
+            int expectedR = 0; //el valor que espera es 0
+            int actualR;
+            actualR = (int)calculo.calcularImpueto(-30);
+            Assert.AreEqual(expectedR, actualR);
+        }
+
+        [TestMethod]
+        public void CacularNumeroExtenso()
+        {
+            CalculoImpuesto calculo = new CalculoImpuesto();
+            int expectedR = 0; //el valor que espera es 0
+            int actualR;
+            actualR = (int)calculo.Sueldo(1234560000);
+            Assert.AreEqual(expectedR, actualR);
+        }
+
+        
+    }
+}
