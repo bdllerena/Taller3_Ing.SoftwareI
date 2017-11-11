4d2abb7078ea38e37c99235f1d6b6022edf5c5f0
Steven Aizaga <sxaizaga@espe.edu.ec>
2017-11-11 11:09:42 -0500

    Taller3

---
+++ Alvarado_Aizaga/ImpuestoRenta_Aizaga_Alvarado/ImpuestoRenta/Calculos.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpuestoRenta
+{
+
+
+   public class Calculos
+    {
+        float valor = 0.9055f;
+        public int CalcularSueldoAnual(float sueldo) {
+            if (sueldo > 0)
+            {
+                float resultado = sueldo * valor * 12;
+                return Convert.ToInt32(Math.Truncate(resultado));
+            }
+            else {
+                Console.WriteLine("Error sueldo incorrecto");
+                return 0;
+            }
+        }
+        public int CalcularImpuesto(float sueldoAnual)
+        {
+
+           int iFraccionBasica = 0;
+           float impuesto= 0;
+
+           if(sueldoAnual>0 && sueldoAnual<=11.290){
+               return 0;
+
+           }
+           if (sueldoAnual >11290 && sueldoAnual <=14390)
+           {
+               iFraccionBasica = 0;
+               impuesto = 0.05f;
+               return Convert.ToInt32(Math.Truncate((((sueldoAnual - 11290 )*impuesto)+iFraccionBasica)));
+           }
+
+           if (sueldoAnual >14390 && sueldoAnual <= 17990)
+           {
+               iFraccionBasica = 155;
+               impuesto = 0.1f;
+               return Convert.ToInt32(Math.Truncate((((sueldoAnual - 14390) * impuesto) + iFraccionBasica)));
+           }
+
+           if (sueldoAnual > 17990 && sueldoAnual <= 21600)
+           {
+               iFraccionBasica = 515;
+               impuesto = 0.12f;
+               return Convert.ToInt32(Math.Truncate((((sueldoAnual - 17990) * impuesto) + iFraccionBasica)));
+           }
+           if (sueldoAnual > 21600 && sueldoAnual <= 43190)
+           {
+               iFraccionBasica = 948;
+               impuesto = 0.15f;
+               return Convert.ToInt32(Math.Truncate((((sueldoAnual - 21600) * impuesto) + iFraccionBasica)));
+           }
+           if (sueldoAnual > 43190 && sueldoAnual <= 64770)
+           {
+               iFraccionBasica = 4187;
+               impuesto = 0.2f;
+               return Convert.ToInt32(Math.Truncate((((sueldoAnual - 43190) * impuesto) + iFraccionBasica)));
+           }
+           if (sueldoAnual > 64770 && sueldoAnual <= 86370)
+           {
+               iFraccionBasica = 8503;
+               impuesto = 0.25f;
+               return Convert.ToInt32(Math.Truncate((((sueldoAnual - 64770) * impuesto) + iFraccionBasica)));
+           }
+           if (sueldoAnual > 86370 && sueldoAnual <= 115140)
+           {
+               iFraccionBasica = 13903;
+               impuesto = 0.3f;
+               return Convert.ToInt32(Math.Truncate((((sueldoAnual - 86370) * impuesto) + iFraccionBasica)));
+           }
+           if (sueldoAnual > 115140)
+           {
+               iFraccionBasica = 22534;
+               impuesto = 0.35f;
+               return Convert.ToInt32(Math.Truncate((((sueldoAnual - 115140) * impuesto) + iFraccionBasica)));
+           }
+         
+
+            return 0;
+        }
+
+    
+
+
+    }
+}
---
+++ Alvarado_Aizaga/ImpuestoRenta_Aizaga_Alvarado/PruebaUnitariaImpuesto/UnitTest1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImpuestoRenta;
+
+namespace PruebaUnitariaImpuesto
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSueldoNormal()
+        {
+            Calculos impuesto = new Calculos();
+            int expected=3803;
+            int actual=impuesto.CalcularSueldoAnual(350);
+            Assert.AreEqual(expected,actual);
+
+        }
+
+        [TestMethod]
+        public void TestSueldoNormal2()
+        {
+            Calculos impuesto = new Calculos();
+            int expected = 10866;
+            int actual = impuesto.CalcularSueldoAnual(1000);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
+        [TestMethod]
+        public void TestSueldoNormal3()
+        {
+            Calculos impuesto = new Calculos();
+            int expected = 11952;
+            int actual = impuesto.CalcularSueldoAnual(1100);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void TestSueldoNormal4()
+        {
+            Calculos impuesto = new Calculos();
+            int expected = 27165;
+            int actual = impuesto.CalcularSueldoAnual(2500);
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void TestSueldoNegativo()
+        {
+            Calculos impuesto = new Calculos();
+            int expected = 0;
+            int actual = impuesto.CalcularSueldoAnual(-10);
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void TestSueldoNulo()
+        {
+            Calculos impuesto = new Calculos();
+            int expected = 0;
+            int actual = impuesto.CalcularSueldoAnual(0);
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void TestImpuesto()
+        {
+            Calculos impuesto = new Calculos();
+            int expected = 0;
+            int actual = impuesto.CalcularImpuesto(impuesto.CalcularSueldoAnual(1000));
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void TestImpuestoR1()
+        {
+            Calculos impuesto = new Calculos();
+            int expected =33;
+            int actual = impuesto.CalcularImpuesto(impuesto.CalcularSueldoAnual(1100));
+            Assert.AreEqual(expected, actual);
+
+        }
+        
+        [TestMethod]
+        public void TestImpuestoR2()
+        {
+            Calculos impuesto = new Calculos();
+            int expected = -33;
+            int actual = impuesto.CalcularImpuesto(impuesto.CalcularSueldoAnual(1100));
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void TestImpuestoR3()
+        {
+            Calculos impuesto = new Calculos();
+            int expected = 1782;
+            int actual = impuesto.CalcularImpuesto(impuesto.CalcularSueldoAnual(2500));
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        //Calculas desde aqui :v
+        [TestMethod]
+        public void TestImpuestoR4()
+        {
+            Calculos impuesto = new Calculos();
+            int expected = 17330;
+            int actual = impuesto.CalcularImpuesto(impuesto.CalcularSueldoAnual(9000));
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void TestImpuestoBig()
+        {
+            Calculos impuesto = new Calculos();
+            int expected = 33;
+            int actual = impuesto.CalcularImpuesto(impuesto.CalcularSueldoAnual(1100000000000000));
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void TestImpuestoR6()
+        {
+            Calculos impuesto = new Calculos();
+            int expected = 33;
+            int actual = impuesto.CalcularImpuesto(impuesto.CalcularSueldoAnual(-1100));
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void TestImpuestoR7()
+        {
+            Calculos impuesto = new Calculos();
+            int expected = 17330;
+            int actual = impuesto.CalcularImpuesto(impuesto.CalcularSueldoAnual(9000));
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
+
+
+
+
+    }
+}
